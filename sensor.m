dc42570f367eef5ed527e0bb8d0874fe5f27db76
yujincheng <yujincheng@yujincheng.me>
2018-10-25 20:16:47 +0800

    finish

--- sensor.m
+++ sensor.m
@@ -1,19 +1,20 @@
-S1 = [4.7, 2.0];
-S2 = [1.6, 1.6];
-S3 = [3.0, 1.5];
-S4 = [1.8, 1.0];
-sigma = 1.6;
-S5 = [3.0, sigma; 1.0, sigma; 2.5, sigma; 0.9, sigma];
+function main
+    S1 = [4.7, 2.0];
+    S2 = [1.6, 1.6];
+    S3 = [3.0, 1.5];
+    S4 = [1.8, 1.0];
+    sigma = 1.6;
+    S5 = [3.0, sigma; 1.0, sigma; 2.5, sigma; 0.9, sigma];
 
-PE = [];
-PE(1,:,:) = [S1; S2; S3; S4; S5(1,:)];
-PE(2,:,:) = [S1; S2; S3; S4; S5(2,:)];
-PE(3,:,:) = [S1; S2; S3; S4; S5(3,:)];
-PE(4,:,:) = [S1; S2; S3; S4; S5(4,:)];
+    PE = [];
+    PE(1,:,:) = [S1; S2; S3; S4; S5(1,:)];
+    PE(2,:,:) = [S1; S2; S3; S4; S5(2,:)];
+    PE(3,:,:) = [S1; S2; S3; S4; S5(3,:)];
+    PE(4,:,:) = [S1; S2; S3; S4; S5(4,:)];
 
-res_dolev = Dolev(PE);
-[bound_sensor,bound_hyp] = Sensor_fuse(PE);
-
+    res_dolev = Dolev(PE);
+    [bound_sensor,bound_hyp] = Sensor_fuse(PE);
+end
 
 %% Dolev
 function res = Dolev(PE)
@@ -29,31 +30,6 @@ function res = Dolev(PE)
     end
 end
 
-
-%% Mahaney and Schnelder's algorithm
-% % true value is in [2.7,2.8], for PE1 delta=4.0, for PE2 delta=2.7 PE3
-% % delta = 1.8 PE4 = 2.0;
-% delta = 2.2;%[4.0, 2.7, 1.8, 2.0];
-% epsilon = 0.1;
-% while (max(res) - min(res) > epsilon)
-%     iter = iter + 1;
-%     for i = 1:4
-%         val = PE(i,:,1);
-%         val = sort(val);
-%         if abs(val(end) - val(1)) > delta
-%             if val(end - 1) - val(1) > delta
-%                 a = [a 1];
-%             else if val(end) - val(2) > delta
-%                 a = [a size(val, 1)];
-%                 end
-%             end
-%         end
-%         mean = (sum(val) - sum(val(a))) / size(val, 1);
-%         val(a) = mean;
-%         res(i) = mean(val);
-%     end 
-% end
-% res
 
 %% Sensor fusion algorithm and Hybrid
 
